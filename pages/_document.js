521068507b593b034e5453b9a824dc1f195c7e42
ayushmaan <ayushmaanrajput768@gmail.com>
2023-07-15 21:23:53 +0530

    managing favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />     
+       </Head>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
